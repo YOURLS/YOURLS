fe2c1531793791cabc40f02a29c5d73be9ac5bda
lesterchan <lesterchan@12232710-3e20-11de-b438-597f59cd7555>
2009-05-12 15:22:27 +0000

    - Moved to /admin/ folder - Rename go.php to yourls-go.php - Rename api.php to yours-api.php - Created login.php placeholder
    git-svn-id: http://yourls.googlecode.com/svn/trunk@7 12232710-3e20-11de-b438-597f59cd7555

--- admin/index.php
+++ admin/index.php
@@ -29,7 +29,7 @@ if ( $link_limit != '' ) {
 } else {
 	$link_filter = '';
 }
-$base_page = 'admin.php';
+$base_page = 'admin/index.php';
 
 ### Searching
 if(!empty($search) && !empty($_GET['s_in'])) {
@@ -127,11 +127,11 @@ $total_pages = ceil($total_items / $perpage);
 	<meta name="copyright" content="Copyright &copy; 2002-<?php echo date('Y'); ?> Lester 'GaMerZ' Chan" />
 	<meta name="author" content="Lester 'GaMerZ' Chan" />
 	<meta name="description" content="Insert URL &laquo; YOURLS &raquo; Your Own URL Shortener' | <?php echo YOURLS_SITE; ?>" />
-	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="css/tablesorter.css" type="text/css" media="screen" />
-	<script src="js/jquery-1.3.1.min.js" type="text/javascript"></script>
-	<script src="js/insert.js" type="text/javascript"></script>
-	<script src="js/jquery.tablesorter.min.js" type="text/javascript"></script>
+	<link rel="stylesheet" href="../css/style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="../css/tablesorter.css" type="text/css" media="screen" />
+	<script src="../js/jquery-1.3.1.min.js" type="text/javascript"></script>
+	<script src="../js/insert.js" type="text/javascript"></script>
+	<script src="../js/jquery.tablesorter.min.js" type="text/javascript"></script>
 </head>
 <body>
 	<h1><a href="<?php echo $base_page; ?>"><span>YOURLS</span>: <span>Y</span>our <span>O</span>wn <span>URL</span> <span>S</span>hortener</a></h1>
--- test-api.php
+++ test-api.php
@@ -5,7 +5,7 @@ $post_data = array( 'url' => 'http://planetozh.com/', 'keyword'=>'ozh', 'format'
 
 // Init the CURL session
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1/ozh.in/api.php");	// URL of the API file
+curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1/ozh.in/yourls-api.php");	// URL of the API file
 curl_setopt($ch, CURLOPT_HEADER, 0);								// No header in the result
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);					// AUTH with any (best) method available
 curl_setopt($ch, CURLOPT_USERPWD, 'ozh:richward');					// login/pwd as defined in config.php
