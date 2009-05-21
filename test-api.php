fc30d75dd8def691823242f87fac0c4cd3ac3cbb
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2009-05-21 11:44:18 +0000

    git-svn-id: http://yourls.googlecode.com/svn/trunk@18 12232710-3e20-11de-b438-597f59cd7555

--- test-api.php
+++ test-api.php
@@ -1,5 +1,9 @@
 <?php
 
+// Auth parameters
+$login = 'joe';
+$pass = '123456';
+
 // POST parameters
 $post_data = array( 'url' => 'http://planetozh.com/', 'keyword'=>'ozh', 'format'=>'json' );
 
@@ -8,7 +12,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1/ozh.in/yourls-api.php");	// URL of the API file
 curl_setopt($ch, CURLOPT_HEADER, 0);								// No header in the result
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);					// AUTH with any (best) method available
-curl_setopt($ch, CURLOPT_USERPWD, 'ozh:richward');					// login/pwd as defined in config.php
+curl_setopt($ch, CURLOPT_USERPWD, "$login:$pass");					// login/pwd as defined in config.php
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);						// Return, do not echo result
 curl_setopt($ch, CURLOPT_POST, 1);									// This is a POST request
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);					// Data to POST
