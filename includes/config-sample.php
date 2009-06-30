21e1bae981fddf0279e8e30006bf7038680903f2
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2009-06-30 20:03:44 +0000

    BCCOMP -> BCMath in readme.html & config sample
    git-svn-id: http://yourls.googlecode.com/svn/trunk@47 12232710-3e20-11de-b438-597f59cd7555

--- includes/config-sample.php
+++ includes/config-sample.php
@@ -54,7 +54,7 @@ define('YOURLS_URL_CONVERT', 36);
  * 62: generate case sensitive keywords (ie: 13jKm or 13JKm)
  * Stick to one setting, don't change after you've created links as it will change all your short URLs!
  * Base 36 should be picked. Use 62 only if you understand what it implies.
- * Using base 62 means you *need* PHP extension BCCOMP
+ * Using base 62 means you *need* PHP extension BCMath
  */
 
 /** 
