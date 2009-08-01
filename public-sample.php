54bd03636b9ff9bf080025b5cfdecf2b4efe622d
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2009-08-01 15:46:34 +0000

    public-sample.php interface
    git-svn-id: http://yourls.googlecode.com/svn/trunk@62 12232710-3e20-11de-b438-597f59cd7555

---
+++ public-sample.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<title>YOURLS Public Interface Sample</title>
+<style>
+body {background:#BFE0FE; color:#666; font:16px/30px verdana,arial,sans-serif;}
+h1 {text-align:center}
+h2 {border-bottom:1px solid white}
+#credits, #credit a {font-size:80%; color:#aaa}
+</style>
+<!-- customize more: CSS, etc -->
+</head>
+
+<body>
+<h1>YOURLS: Your Own URL Shortener</h1>
+
+<?php
+
+// Part to be executed if FORM has been submitted
+if ( isset($_REQUEST['url']) ) {
+
+	$url = $_REQUEST['url'];
+	$keyword = isset( $_REQUEST['keyword'] ) ? $_REQUEST['keyword'] : '' ;
+
+	require_once( dirname(__FILE__).'/includes/config.php' );
+	$db = yourls_db_connect();
+
+	$return = yourls_add_new_link( $url, $keyword, $db );
+	
+	$shorturl = $return['shorturl'];
+	$message = $return['message'];
+	
+	echo <<<RESULT
+	<h2>URL has been shortened</h2>
+	<p>Original URL: <code><a href="$url">$url</a></code></p>
+	<p>Short URL: <code><a href="$shorturl">$shorturl</a></code></p>
+	<p>$message</p>
+RESULT;
+
+
+// Part to be executed when no form has been submitted
+} else {
+
+	echo <<<HTML
+	<h2>Enter a new URL to shorten</h2>
+	<form method="post" action="">
+	<p><label>URL: <input type="text" name="url" value="http://" size="50" /></label></p>
+	<p><label>Optional custom keyword: <input type="text" name="keyword" size="5" /></label></p>
+	<p><input type="submit" value="Shorten" /></p>
+	</form>	
+HTML;
+
+}
+
+?>
+
+<p id="credits">Powered by <a href="http://yourls.org/" title="YOURLS: Your Own URL Shortener">YOURLS</a></p>
+</body>
+</html>
