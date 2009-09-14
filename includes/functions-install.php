63b68d9a6d7a6e3e53941c06d0211c16ef42f472
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2009-09-14 18:33:17 +0000

    Typo in .htaccess. Fixes issue 71.
    git-svn-id: http://yourls.googlecode.com/svn/trunk@156 12232710-3e20-11de-b438-597f59cd7555

--- includes/functions-install.php
+++ includes/functions-install.php
@@ -75,7 +75,7 @@ function yourls_create_htaccess() {
 		'RewriteCond %{REQUEST_FILENAME} !-f',
 		'RewriteCond %{REQUEST_FILENAME} !-d',
 		'RewriteRule ^([0-9A-Za-z]+)/?$ '.$path.'/yourls-go.php?id=$1 [L]',
-		'RewriteRule ^([0-9A-Za-z]+)\+/?$ '.$path.'/yourls-info.php?id=$1 [L]',
+		'RewriteRule ^([0-9A-Za-z]+)\+/?$ '.$path.'/yourls-infos.php?id=$1 [L]',
 		'</IfModule>',
 	);
 	
