1e8226f959353585515136e9aa3f7a746d1a235c
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2009-10-12 21:57:25 +0000

    Fixes issue 128.
    git-svn-id: http://yourls.googlecode.com/svn/trunk@227 12232710-3e20-11de-b438-597f59cd7555

--- includes/functions-upgrade.php
+++ includes/functions-upgrade.php
@@ -22,7 +22,7 @@ function yourls_upgrade_to_14( $step ) {
 		$clean = yourls_clean_htaccess_for_14(); // returns bool
 		$create = yourls_create_htaccess(); // returns bool
 		if ( !$create )
-			echo "<p>Manually check your <tt>.htaccess</tt> file. Please refer to <a href='http://yourls.org/htaccess'>http://yourls.org/htaccess</a>.";
+			echo "<p class='warning'>Please create your <tt>.htaccess</tt> file (I could not do it for you). Please refer to <a href='http://yourls.org/htaccess'>http://yourls.org/htaccess</a>.";
 		yourls_redirect_javascript( YOURLS_SITE."/admin/upgrade.php?step=2&oldver=1.3&newver=1.4&oldsql=100&newsql=200", $result );
 		break;
 		
