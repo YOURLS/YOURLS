bb2d19aa34fc36fdd25ddb49003c38ace9a08f70
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2009-10-14 20:41:41 +0000

    Remove unused function
    git-svn-id: http://yourls.googlecode.com/svn/trunk@231 12232710-3e20-11de-b438-597f59cd7555

--- includes/functions-infos.php
+++ includes/functions-infos.php
@@ -167,23 +167,6 @@ function yourls_stats_get_best_day( $list_of_days ) {
 	}
 }
 
-// Fetch remote page title
-function yourls_get_page_title( $url, $default = '' ) {
-	if ( !class_exists('DOMDocument') )
-		return $default;
-
-	$dom = new DOMDocument();
-
-	if( @$dom->loadHTMLFile('http://planetozh.com/') ) {
-		$list = $dom->getElementsByTagName("title");
-		if ($list->length > 0) {
-			return( $list->item(0)->textContent );
-		}
-	}
-	
-	return $default;
-}
-
 // Return domain of a URL
 function yourls_get_domain( $url, $include_scheme = false ) {
 	$parse = parse_url( $url );
