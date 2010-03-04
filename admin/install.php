261c23c3057ce692ccd03dc342069e5084fd75a3
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2010-03-04 10:45:49 +0000

    Typo
    git-svn-id: http://yourls.googlecode.com/svn/trunk@281 12232710-3e20-11de-b438-597f59cd7555

--- admin/install.php
+++ admin/install.php
@@ -66,7 +66,7 @@ yourls_html_head( 'install' );
 				echo '<p>&nbsp;</p><p style="text-align: center;"><input type="submit" name="install" value="Install YOURLS" class="button" /></p>';
 			} else {
 				if( count($error) == 0 )
-					echo '<p>&nbsp;</p><p style="text-align: center;">&raquo; <a href="'.yourls_admin_url().'" title="YOURS Administration Page">YOURS Administration Page</a></p>';
+					echo '<p>&nbsp;</p><p style="text-align: center;">&raquo; <a href="'.yourls_admin_url().'" title="YOURLS Administration Page">YOURLS Administration Page</a></p>';
 			}
 		?>
 	</form>
