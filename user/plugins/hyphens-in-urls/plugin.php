1e76956efd6a7865157ecb0cbcfc014e6930ba37
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2010-06-25 12:53:15 +0000

    Revert to a-zA-Z0-9 charset in short url keywords, but bundle a core plugin allowing hyphens. Fixes issue 80. Fixes issue 103. Fixes issue 292.
    git-svn-id: http://yourls.googlecode.com/svn/trunk@412 12232710-3e20-11de-b438-597f59cd7555

---
+++ user/plugins/hyphens-in-urls/plugin.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Allow Hyphens in Short URLs
+Plugin URI: http://yourls.org/
+Description: Allow hyphens in short URLs (like <tt>http://sho.rt/hello-world</tt>)
+Version: 1.0
+Author: Ozh
+Author URI: http://ozh.org/
+*/
+
+yourls_add_filter( 'get_shorturl_charset', 'ozh_hyphen_in_charset' );
+function ozh_hyphen_in_charset( $in ) {
+	return $in.'-';
+}
+
+
+?>
