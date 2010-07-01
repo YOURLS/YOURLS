672356e0f9f6f0bf3061a775563a2438c9002bee
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2010-07-01 08:28:49 +0000

    More comments
    git-svn-id: http://yourls.googlecode.com/svn/trunk@434 12232710-3e20-11de-b438-597f59cd7555

--- yourls-api.php
+++ yourls-api.php
@@ -9,6 +9,7 @@ yourls_do_action( 'api', $action );
 	
 switch( $action ) {
 
+	// Shorten a URL
 	case 'shorturl':
 		$url = ( isset( $_REQUEST['url'] ) ? $_REQUEST['url'] : '' );
 		$keyword = ( isset( $_REQUEST['keyword'] ) ? $_REQUEST['keyword'] : '' );
@@ -18,22 +19,26 @@ switch( $action ) {
 		unset( $return['html'] ); // in API mode, no need for our internal HTML output
 		break;
 	
+	// Global stats
 	case 'stats':
 		$filter = ( isset( $_REQUEST['filter'] ) ? $_REQUEST['filter'] : '' );
 		$limit = ( isset( $_REQUEST['limit'] ) ? $_REQUEST['limit'] : '' );
 		$return = yourls_api_stats( $filter, $limit );
 		break;
-		
+	
+	// Stats for a shorturl
 	case 'url-stats':
 		$shorturl = ( isset( $_REQUEST['shorturl'] ) ? $_REQUEST['shorturl'] : '' );
 		$return = yourls_api_url_stats( $shorturl );
 		break;
 
+	// Expand a short link
 	case 'expand':
 		$shorturl = ( isset( $_REQUEST['shorturl'] ) ? $_REQUEST['shorturl'] : '' );
 		$return = yourls_api_expand( $shorturl );
 		break;
-		
+	
+	// Missing or incorrect action parameter
 	default:
 		$return = array(
 			'errorCode' => 400,
