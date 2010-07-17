ec6e6de8043d7de267c6dd373d322865a6966684
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2010-07-17 13:37:27 +0000

    Don't strip out query string, this breaks Prefix-n-Shorten
    git-svn-id: http://yourls.googlecode.com/svn/trunk@455 12232710-3e20-11de-b438-597f59cd7555

--- yourls-loader.php
+++ yourls-loader.php
@@ -12,11 +12,13 @@ require_once( dirname(__FILE__).'/includes/load-yourls.php' );
 // Get request in YOURLS base (eg in 'http://site.com/yourls/abcd' get 'abdc')
 $scheme = ( isset($_SERVER["HTTPS"]) ? 'https' : 'http' );
 $request = str_replace( YOURLS_SITE.'/', '', $scheme . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
-if( isset($_SERVER['QUERY_STRING']) )
-	$request = str_replace( '?'.$_SERVER['QUERY_STRING'], '', $request );
+
 /**
  * TODO: think about doing something with $_SERVER['QUERY_STRING']?
- * (if so, deal with logout requests early)
+ * Like sho.rt/keyword?something
+ * if so:
+ *     - deal with logout requests early
+ *     - handle differently short urls (sho.rt/keyword?something) and prefix-n-shorten (sho.rt/http://bleh/?s=1)
  */
 
 // Make valid regexp pattern from authorized charset in keywords
