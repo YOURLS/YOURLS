b298e026db1144036a77f9c273a7f104727d8f99
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2010-11-06 21:26:18 +0000

    Another bundled plugin with random pretty background, because I like it.
    git-svn-id: http://yourls.googlecode.com/svn/trunk@524 12232710-3e20-11de-b438-597f59cd7555

---
+++ user/plugins/random-bg/plugin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Random Backgrounds
+Plugin URI: http://yourls.org/
+Description: Pretty random background patterns
+Version: 1.0
+Author: Ozh
+Author URI: http://ozh.org/
+*/
+
+// Add the inline style
+yourls_add_action( 'html_head', 'ozh_yourls_randombg' );
+function ozh_yourls_randombg() {
+	$bg = glob( dirname( __FILE__ ).'/img/*png' );
+	$url = yourls_plugin_url( dirname( __FILE__ ) );
+	$rnd = yourls_plugin_url( $bg[ mt_rand( 0, count( $bg ) - 1 ) ] );
+	echo <<<CSS
+<style type="text/css">
+		body {background:#e3f3ff url($rnd) repeat;}
+	</style>
+
+CSS;
+}
+
