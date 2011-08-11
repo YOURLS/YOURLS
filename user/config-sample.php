6d0fa3c05dddd2584a22137726592a87c349f874
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2011-08-11 12:40:19 +0000

    More comments about YOURLS_PRIVATE
    git-svn-id: http://yourls.googlecode.com/svn/trunk@663 12232710-3e20-11de-b438-597f59cd7555

--- user/config-sample.php
+++ user/config-sample.php
@@ -38,7 +38,9 @@ define( 'YOURLS_HOURS_OFFSET', 0 );
  ** Set to false to allow multiple short URLs pointing to the same long URL (bit.ly behavior) */
 define( 'YOURLS_UNIQUE_URLS', true );
 
-/** Private means protected with login/pass as defined below. Set to false for public usage on a restricted intranet or for test setups. */
+/** Private means the Admin area will be protected with login/pass as defined below.
+ ** Set to false for public usage (eg on a restricted intranet or for test setups)
+ ** Read http://yourls.org/privatepublic for more details if you're unsure */
 define( 'YOURLS_PRIVATE', true );
 
 /** A random secret hash used to encrypt cookies. You don't have to remember it, make it long and complicated. Hint: copy from http://yourls.org/cookie **/
