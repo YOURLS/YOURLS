5db14d1804881ff370b85210e66eb76ba085e40c
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2011-08-29 15:19:37 +0000

    Rename example page to avoid overriding an actual page.
    git-svn-id: http://yourls.googlecode.com/svn/trunk@681 12232710-3e20-11de-b438-597f59cd7555

--- pages/about.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "This is an About page. Its URL is simply ". YOURLS_SITE ."/about";
-
-?>
---
+++ pages/examplepage.php
@@ -0,0 +1,18 @@
+<?php
+
+// Make sure we're in YOURLS context
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+// Display page content. Any PHP, HTML and YOURLS function can go here.
+$url = YOURLS_SITE . '/examplepage';
+
+yourls_html_head( 'examplepage', 'Example page' );
+
+?>
+
+<p>This is an example page. Its URL is simply <?php echo $url; ?></p>
+
+<?php
+
+yourls_html_footer();
+
