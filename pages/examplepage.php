cf7b18350b2ae40bda0c89abef94e58c05d29ab4
ozhozh <ozhozh@12232710-3e20-11de-b438-597f59cd7555>
2012-08-17 13:54:20 +0000

    Better example of user page
    git-svn-id: http://yourls.googlecode.com/svn/trunk@735 12232710-3e20-11de-b438-597f59cd7555

--- pages/examplepage.php
+++ pages/examplepage.php
@@ -1,7 +1,12 @@
 <?php
 
 // Make sure we're in YOURLS context
-if( !defined( 'YOURLS_ABSPATH' ) ) die();
+if( !defined( 'YOURLS_ABSPATH' ) ) {
+	// Attempt to guess URL via YOURLS
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . str_replace( array( '/pages/', '.php' ) , array ( '/', '' ), $_SERVER['REQUEST_URI'] );
+	echo "Try this instead: <a href='$url'>$url</a>";
+	die();
+}
 
 // Display page content. Any PHP, HTML and YOURLS function can go here.
 $url = YOURLS_SITE . '/examplepage';
