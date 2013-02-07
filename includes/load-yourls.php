4ad40a57fd87c4e677717fad987570400a74f257
Ozh <ozh@ozh.org>
2013-02-07 20:32:48 +0000

    More portable stricter error reporting.
    git-svn-id: http://yourls.googlecode.com/svn/trunk@851 12232710-3e20-11de-b438-597f59cd7555

--- includes/load-yourls.php
+++ includes/load-yourls.php
@@ -94,7 +94,7 @@ if( !defined( 'YOURLS_DEBUG' ) )
 	
 // Error reporting
 if( defined( 'YOURLS_DEBUG' ) && YOURLS_DEBUG == true ) {
-	error_reporting( E_ALL | E_STRICT );
+	error_reporting( -1 );
 } else {
 	error_reporting( E_ERROR | E_PARSE );
 }
