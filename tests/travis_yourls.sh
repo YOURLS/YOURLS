674e9e51b36a33c298a805c02b976486e3452092
LeoColomb <leocolombaro@yahoo.fr>
2013-05-02 17:28:01 +0200

    Del sh

--- tests/travis_yourls.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install YOURLS
-mysql -e "create database IF NOT EXISTS yourls;" -uroot;
-git clone git://github.com/YOURLS/YOURLS.git
-sh -c "cd YOURLS"
-git checkout master
-sh -c "cp ../yourls-tests-config-travis.php user/config.php"
