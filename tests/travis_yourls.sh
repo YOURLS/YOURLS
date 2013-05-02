321bfc87f5583dd9c2fb19ca2b1b6404f210bf67
Léo Colombaro <leocolombaro@yahoo.fr>
2013-05-02 17:09:45 +0300

    Update travis_yourls.sh

--- tests/travis_yourls.sh
+++ tests/travis_yourls.sh
@@ -3,6 +3,6 @@
 # Install YOURLS
 mysql -e "create database IF NOT EXISTS yourls;" -uroot;
 git clone git://github.com/YOURLS/YOURLS.git
-cd YOURLS
+sh -c "cd YOURLS"
 git checkout master
-copy ../yourls-tests-config-travis.php user/config.php
+sh -c "cp ../yourls-tests-config-travis.php user/config.php"
