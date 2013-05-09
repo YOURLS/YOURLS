1c93ba83202ca94ba152c6377cea372a62e28abb
LeoColomb <leocolombaro@yahoo.fr>
2013-05-10 01:24:08 +0200

    Fix functions for theming

--- includes/functions-themes.php
+++ includes/functions-themes.php
@@ -21,35 +21,35 @@
  */
 function yourls_html_template_content( $template_part ) {
 	// Collect additional optional arguments, for instance the page context ('admin', 'plugins'...)
-    $args = func_get_args();
-    
+	$args = func_get_args();
+	
 	// Page structure
-    $elements = array (
-        'before' => array(
-            'sidebar_start',
-            'html_logo',
-            [ 'html_menu', array( $args[1] ) ],
-            'html_footer',
-            'sidebar_end',
-            'wrapper_start',   // themes should probably not remove this
-        ),
-        'after' => array(
-            'wrapper_end',     // themes should probably not remove this
-        )
-    );
-    
-    // Allow theming!
-    $elements = yourls_apply_filter( 'html_template_content', $elements, $template_part, $args );
-    
+	$elements = array (
+		'before' => array(
+			'yourls_sidebar_start',
+			'yourls_html_logo',
+			[ 'yourls_html_menu', array( $args[1] ) ],
+			'yourls_html_footer',
+			'yourls_sidebar_end',
+			'yourls_wrapper_start',   // themes should probably not remove this
+		),
+		'after' => array(
+			'yourls_wrapper_end',     // themes should probably not remove this
+		)
+	);
+	
+	// Allow theming!
+	$elements = yourls_apply_filter( 'html_template_content', $elements, $template_part, $args );
+	
 	// 'Draw' page
-    foreach( $elements[ $template_part ] as $element ) {
-        if( is_array( $element ) ) {
-            call_user_func_array( 'yourls_' . $element[0], $element[1] );
-        } else {
-            call_user_func_array( 'yourls_' . $element, array() );
-        }
-    }
-    
-    if( $template_part == 'after' );
-        yourls_html_ending();
+	foreach( $elements[ $template_part ] as $element ) {
+		if( is_array( $element ) ) {
+			call_user_func_array( $element[0], $element[1] );
+		} else {
+			call_user_func_array( $element, array() );
+		}
+	}
+	
+	if( $template_part == 'after' );
+		yourls_html_ending();
 }
