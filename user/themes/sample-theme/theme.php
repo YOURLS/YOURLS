73f536be1faacdd8067e51e331881732fca0b586
ozh <ozh@ozh.org>
2013-05-15 23:36:05 +0200

    Sample themes! A simple one, an advanced one.

---
+++ user/themes/sample-theme/theme.php
@@ -0,0 +1,15 @@
+<?php
+/* Optional function file for your theme.
+
+   If you theme needs to define custom hooks or special behavior,
+   you can put them here.
+
+   Suggested read:
+   https://github.com/YOURLS/YOURLS/wiki/Themes
+   https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
+   https://github.com/YOURLS/YOURLS/wiki#for-developpers
+   
+   Have fun!
+*/
+
+yourls_add_notice( 'this theme is annoying' );
