6c5e91d4e6b5989af72bb3b92b552cd3ff27c525
LeoColomb <leocolombaro@yahoo.fr>
2013-09-02 13:16:37 +0200

    Hum... Not the good path
    I'm hungry.

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -7,9 +7,9 @@ require_once 'PHPUnit/Autoload.php';
 
 // Include config
 if ( defined( 'TRAVIS_TESTSUITE' ) && TRAVIS_TESTSUITE == true )
-	$config_file_path = dirname( dirname( __FILE__ ) ) . '/user/config.php';
+	$config_file_path = dirname( dirname( dirname( __FILE__ ) ) ) . '/user/config.php';
 else 
-	$config_file_path = dirname( dirname( dirname( __FILE__ ) ) ) . '/yourls-tests-config.php';
+	$config_file_path = dirname( dirname( __FILE__ ) ) . '/yourls-tests-config.php';
 if ( !is_readable( $config_file_path ) ) {
 	die( "ERROR: yourls-tests-config.php is missing!\n" );
 }
