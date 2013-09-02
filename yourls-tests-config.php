9c8d01ed0d07bf47230071f4b6803d82431e3d4c
LeoColomb <leocolombaro@yahoo.fr>
2013-09-02 09:46:25 +0200

    Add default lang

--- yourls-tests-config.php
+++ yourls-tests-config.php
@@ -12,7 +12,7 @@ if( !defined( 'YOURLS_TESTS_CI' ) || YOURLS_TESTS_CI === false ) {
 
 	define( 'YOURLS_SITE', 'http://localhost/YOURLS' );
 	define( 'YOURLS_HOURS_OFFSET', 0 ); 
-	define( 'YOURLS_LANG', '' ); 
+	define( 'YOURLS_LANG', 'fr_FR' ); 
 	define( 'YOURLS_UNIQUE_URLS', true );
 	define( 'YOURLS_PRIVATE', true );
 	define( 'YOURLS_COOKIEKEY', 'Op@G)SqI~sNQtBqv|8}0(gqM}Ft&g-n&|tVZR2B$' );
