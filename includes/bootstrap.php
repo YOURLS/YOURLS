85ed214ef9ab15e36398a432fcbb32b50ed6ea74
ozh <ozh@ozh.org>
2013-09-04 17:18:42 +0200

    Trim down config file to the bare minimum
    So we don't have to maintain `yourls-tests-config-sample.php` and `yourls-tests-config-travis.php`. If we have any setting or config to modify -> `bootstrap.php`

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -8,13 +8,13 @@ require_once dirname( __FILE__ ) . '/utils.php';
 
 // Include config
 if ( defined( 'TRAVIS_TESTSUITE' ) && TRAVIS_TESTSUITE == true )
-	$config_file_path = dirname( dirname( dirname( __FILE__ ) ) ) . '/user/config.php';
+	define( 'YOURLS_CONFIGFILE', dirname( dirname( dirname( __FILE__ ) ) ) . '/user/config.php' ); // this is a copy of yourls-tests-config-travis.php
 else 
-	$config_file_path = dirname( dirname( __FILE__ ) ) . '/yourls-tests-config.php';
-if ( !is_readable( $config_file_path ) ) {
+	define( 'YOURLS_CONFIGFILE', dirname( dirname( __FILE__ ) ) . '/yourls-tests-config.php' );
+if ( !is_readable( YOURLS_CONFIGFILE ) ) {
 	die( "ERROR: yourls-tests-config.php is missing!\n" );
 }
-require_once $config_file_path;
+require_once YOURLS_CONFIGFILE;
 
 // Globalize some YOURLS variables because PHPUnit loads this inside a function
 // See https://github.com/sebastianbergmann/phpunit/issues/325
@@ -24,7 +24,33 @@ global $ydb, $yourls_user_passwords, $yourls_reserved_URL,        // main object
        $yourls_allowedentitynames, $yourls_allowedprotocols,      // used by KSES
 	   $ezsql_mysql_str, $ezsql_mysqli_str, $ezsql_pdo_str;       // used by ezSQL
 
+// Initialize ourselves some constants that are typically user defined
+$yourls_user_passwords = array(
+	'yourls'  => 'travis-ci-test',
+	'clear'   => 'somepassword',
+	'md5'     => 'md5:31712:f6cae1f032b9ae81b233866f4aa791af', // password: "md5"
+	'phpass'  => '$2a$08$UbOIKE2oyh.shrjSkOJ3Au7zN2vqTkrhsmAFgaMPomfeS0S6xHjG6', // password: "phpass"
+	'phpass2' => '!2a!08$zzwkOxZHwup7qsfSuxdFXOzRBEOtKu4b15gXqceYJ23GOJtRq.yvO', // password: also "phpass" with YOURLS' internal char substitution
+);
+$yourls_reserved_URL = array(
+	'porn', 'faggot', 'sex', 'nigger', 'fuck', 'cunt', 'dick', 'gay',
+);
+$yourls_user_consts = array(
+	'YOURLS_HOURS_OFFSET'     => 5,
+	'YOURLS_UNIQUE_URLS'      => true,
+	'YOURLS_PRIVATE'          => true,
+	'YOURLS_COOKIEKEY'        => 'I &hearts; unit tests',
+	'YOURLS_URL_CONVERT'      => 62,
+	'YOURLS_DB_PREFIX'        => 'yourls_',
+	'YOURLS_NO_HASH_PASSWORD' => true, // prevents rewriting config.php with encrypted passwords
+	'YOURLS_API'              => true, // prevents all internal redirections (login forms, etc)
+);
+foreach( $yourls_user_consts as $CONST => $value ) {
+	if( !defined( $CONST ) )
+		define( $CONST, $value );
+}
 
+// All set -- go.
 declare_yourls_consts();
 load_yourls();
 drop_all_tables_if_local();
--- yourls-tests-config-travis.php
+++ yourls-tests-config-travis.php
@@ -1,29 +1,20 @@
 <?php
+/**
+ * YOURLS Config for Travis (https://travis-ci.org/YOURLS/YOURLS)
+ */
 if( !defined( 'YOURLS_TESTS_CI' ) || YOURLS_TESTS_CI === false ) {
 	define( 'YOURLS_TESTS_CI', true );
 
+	/*** Code base and URL of that code base */
 	define( 'YOURLS_ABSPATH', dirname( dirname( __FILE__ ) ) );
+	define( 'YOURLS_SITE', 'http://localhost/YOURLS' );
 
+	/*** MySQL settings */
 	define( 'YOURLS_DB_USER', 'root' );
 	define( 'YOURLS_DB_PASS', '' );
 	define( 'YOURLS_DB_NAME', 'yourls_tests' );
 	define( 'YOURLS_DB_HOST', 'localhost' );
-	define( 'YOURLS_DB_PREFIX', 'yourls_' );
 
-	define( 'YOURLS_SITE', 'http://localhost/YOURLS' );
-	define( 'YOURLS_HOURS_OFFSET', 0 ); 
+	/*** Site options */
 	define( 'YOURLS_LANG', 'fr_FR' ); 
-	define( 'YOURLS_UNIQUE_URLS', true );
-	define( 'YOURLS_PRIVATE', true );
-	define( 'YOURLS_COOKIEKEY', 'Op@G)SqI~sNQtBqv|8}0(gqM}Ft&g-n&|tVZR2B$' );
-
-	$yourls_user_passwords = array(
-	  'yourls' => 'travis-ci-test'
-	);
-
-	define( 'YOURLS_URL_CONVERT', 62 );
-
-	$yourls_reserved_URL = array(
-		'porn', 'faggot', 'sex', 'nigger', 'fuck', 'cunt', 'dick', 'gay',
-	);
 }
