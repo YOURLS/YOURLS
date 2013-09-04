c7da5d5ad9041e36d0ad6c2c35932671cce5088b
ozh <ozh@ozh.org>
2013-09-04 19:42:42 +0200

    Correct (?) path depending on if Travis on YOURLS/YOURLS or YOURLS/YOURLS-unit-tests

--- yourls-tests-config-travis.php
+++ yourls-tests-config-travis.php
@@ -6,7 +6,11 @@ if( !defined( 'YOURLS_TESTS_CI' ) || YOURLS_TESTS_CI === false ) {
 	define( 'YOURLS_TESTS_CI', true );
 
 	/*** Code base and URL of that code base */
-	define( 'YOURLS_ABSPATH', '/home/travis/build/YOURLS/YOURLS' );
+	if( defined( 'TRAVIS_REPO_SLUG' ) ) {
+		define( 'YOURLS_ABSPATH', '/home/travis/build/' . TRAVIS_REPO_SLUG;
+	} else {
+		die( 'Not in Travis' );
+	}
 	define( 'YOURLS_SITE', 'http://localhost/YOURLS' );
 
 	/*** MySQL settings */
