87ee61b11cbd8a667d9e2f39b9d2635d9f80697b
ozh <ozh@ozh.org>
2013-09-06 17:10:33 +0200

    Geo IP function fix, for IPv4 and IPv6. Fixes #1482

--- includes/functions.php
+++ includes/functions.php
@@ -858,23 +858,43 @@ function yourls_do_log_redirect() {
 /**
  * Converts an IP to a 2 letter country code, using GeoIP database if available in includes/geo/
  *
+ * @since 1.4
+ * @param string $ip IP or, if empty string, will be current user IP
+ * @param string $defaut Default string to return if IP doesn't resolve to a country (malformed, private IP...)
+ * @return string 2 letter country code (eg 'US') or $default
  */
 function yourls_geo_ip_to_countrycode( $ip = '', $default = '' ) {
 	// Allow plugins to short-circuit the Geo IP API
 	$location = yourls_apply_filter( 'shunt_geo_ip_to_countrycode', false, $ip, $default ); // at this point $ip can be '', check if your plugin hooks in here
 	if ( false !== $location )
 		return $location;
-
-	if ( !file_exists( YOURLS_INC.'/geo/GeoIP.dat') || !file_exists( YOURLS_INC.'/geo/geoip.inc') )
-		return $default;
-
+	
 	if ( $ip == '' )
 		$ip = yourls_get_IP();
 	
-	require_once( YOURLS_INC.'/geo/geoip.inc') ;
-	$gi = geoip_open( YOURLS_INC.'/geo/GeoIP.dat', GEOIP_STANDARD);
-	$location = geoip_country_code_by_addr($gi, $ip);
-	geoip_close($gi);
+	// Use IPv4 or IPv6 DB & functions
+	if( false === filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 ) ) {
+		$db   = 'GeoIP.dat';
+		$func = 'geoip_country_code_by_addr';
+	} else {
+		$db   = 'GeoIPv6.dat';
+		$func = 'geoip_country_code_by_addr_v6';
+	}
+	
+	if ( !file_exists( YOURLS_INC . '/geo/' . $db ) || !file_exists( YOURLS_INC .'/geo/geoip.inc' ) )
+		return $default;
+
+	require_once( YOURLS_INC . '/geo/geoip.inc' );
+	$gi = geoip_open( YOURLS_INC . '/geo/' . $db, GEOIP_STANDARD );
+	try {
+		$location = call_user_func( $func, $gi, $ip );
+	} catch ( Exception $e ) {
+		$location = '';
+	}
+	geoip_close( $gi );
+	
+	if( '' == $location )
+		$location = $default;
 
 	return yourls_apply_filter( 'geo_ip_to_countrycode', $location, $ip, $default );
 }
