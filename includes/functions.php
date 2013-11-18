c07fc88fd0b89e533b2d0368b0a71aa11b22f89e
Clayton Daley <clayton.daley@gmail.com>
2013-11-17 22:22:52 -0500

    Updated function name. Deprecated old version. Updated yourls-infos.php.

--- includes/functions.php
+++ includes/functions.php
@@ -1216,16 +1216,21 @@ function yourls_allow_duplicate_longurls() {
 }
 
 /**
- * Return list of all shorturls associated to the same long URL. Returns NULL or array of keywords.
+ * @deprecated Return list of all shorturls associated to the same long URL. Returns NULL or array of keywords.
  *
  */
 function yourls_get_duplicate_keywords( $longurl ) {
+	yourls_deprecated_function( __FUNCTION__, 1.7, 'yourls_get_longurl_keywords' );
 	if( !yourls_allow_duplicate_longurls() )
 		return NULL;
 	return yourls_apply_filter( 'get_duplicate_keywords', yourls_get_keywords ( $longurl ), $longurl );
 }
 
-function yourls_get_keywords( $longurl, $sort = 'none', $order = 'ASC' ) {
+/**
+ * Return list of keywords that redirect to the submitted long URL. Returns array of keywords.
+ *
+ */
+function yourls_get_longurl_keywords( $longurl, $sort = 'none', $order = 'ASC' ) {
 	global $ydb;
 	$longurl = yourls_escape( yourls_sanitize_url($longurl) );
 	$table = YOURLS_DB_TABLE_URL;
