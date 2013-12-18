9653aa501ca78c3c0151a4d142d228371a335757
ozh <ozh@ozh.org>
2013-12-18 23:49:31 +0100

    (we're in the middle of something -- dont use)

--- includes/functions-update.php
+++ includes/functions-update.php
@@ -9,31 +9,41 @@
  * Check if there's a newer version of YOURLS
  *
  * This function collects various stats to help us improve YOURLS. See https://gist.github.com/ozh/5518761
- * Results of requests sent to api.yourls.org are stored in option 'core_version_checks' and is an array
- * with the following keys:
+ * Results of requests sent to api.yourls.org are stored in option 'core_version_checks' and is an object
+ * with the following properties:
  *    - failed_attempts : number of consecutive failed attempts
  *    - last_attempt    : time() of last attempt
- *    - last_result     : last content retrieved from api.yourls.org
+ *    - last_result     : content retrieved from api.yourls.org during previous check
+ *    - version_checked : installed YOURLS version that was last checked
  *
  * @since 1.7
  * @param unknown_type $a    TODO
  * @return unknown           TODO
  */
-function yourls_version_check() {
+function yourls_check_core_version() {
 
 	if( defined( 'YOURLS_NO_VERSION_CHECK' ) && YOURLS_NO_VERSION_CHECK )
 		return false;
 		
 	global $ydb, $yourls_user_passwords;
 	
+	$checks = yourls_get_option( 'core_version_checks' );
+	
+	// Invalidate check data when YOURLS version changes
+	if ( is_object( $checks ) && YOURLS_VERSION != $checks->version_checked )
+		$checks = false;
+
+	if( !is_object( $checks ) ) {
+		$checks = new stdClass;
+		$checks->failed_attempts = 0;
+		$checks->last_attempt    = 0;
+		$checks->last_result     = '';
+		$checks->version_checked = YOURLS_VERSION;
+	}
+
 	// Config file location ('/user' or '/includes')
 	$conf_loc = str_replace( YOURLS_ABSPATH, '', YOURLS_CONFIGFILE );
 	$conf_loc = str_replace( '/config.php', '', $conf_loc );
-	
-	$checks = yourls_get_option( 'core_version_checks' );
-	if( !$checks ) {
-		$checks['failed_attempts'] = 0;
-	}
 		
 	// The collection of stuff to report
 	$stuff = array(
@@ -65,12 +75,12 @@ function yourls_version_check() {
 	// Send it in
 	$url = 'https://api.yourls.org/core/version/1.0/';
 	$req = yourls_http_post( $url, array(), $stuff );
-	$checks['last_attempt'] = time();
+	$checks->last_attempt = time();
 	
 	// Unexpected results ?
 	if( is_string( $req ) or !$req->success ) {
-		$checks['failed_attempts']++;
-		$checks['last_result'] = '';
+		$checks->failed_attempts++;
+		$checks->last_result  = '';
 		yourls_update_option( 'core_version_checks', $checks );
 		return false;
 	}
@@ -79,8 +89,8 @@ function yourls_version_check() {
 	$json = json_decode( trim( $req->body ) );
 	
 	// All went OK - mark this down!
-	$checks['failed_attempts'] = 0;
-	$checks['last_result']     = $json;
+	$checks->failed_attempts = 0;
+	$checks->last_result     = $json;
 	yourls_update_option( 'core_version_checks', $checks );
 	
 	return true;
@@ -96,7 +106,33 @@ function yourls_version_check() {
  * @param unknown_type $a    TODO
  * @return unknown           TODO
  */
-function yourls_maybe_check_version() {
+function yourls_maybe_check_core_version() {
 
+	/* We check if we're viewing an admin page and one of these cases:
+	 - no check data (never checked)
+	 - failed_attempts = 0 && last_attempt > 24h  ( 24 * 3600 > ( time() - $check->last_attempt )
+	 - failed_attempts > 0 && last_attempt >  2h
+	 - version_checked != YOURLS_VERSION
+	 
+	 In the future, check with cronjob emulation instead of limiting to when viewing an admin page
+	*/
+	
+	== blah i was last here ==
+
+	$checks = yourls_get_option( 'core_version_checks' );
+	
+	if( !$checks )
+		yourls_check_core_version();
+		
+	
+		
+		
+	if ( isset( $$checks->last_checked ) &&
+        12 * HOUR_IN_SECONDS > ( time() - $current->last_checked ) &&
+        isset( $current->version_checked ) &&
+        $current->version_checked == $wp_version )
+        return;
+
+	yourls_check_core_version();
 
 }
