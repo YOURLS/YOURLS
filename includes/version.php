0e2fa9be6cf64ca33790f6c060852285ca998d3d
ozh <ozh@ozh.org>
2014-01-09 21:46:53 +0100

    Bump to 1.7

--- includes/version.php
+++ includes/version.php
@@ -3,7 +3,7 @@
  * YOURLS version
  *
  */
-define( 'YOURLS_VERSION', '1.7-RC1' );
+define( 'YOURLS_VERSION', '1.7' );
 
 /**
  * YOURLS DB version. Increments when changes are made to the DB schema, to trigger a DB update
