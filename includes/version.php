6fb00e397af8562eb13c4fe3a10ac3c702b88866
ozh <ozh@ozh.org>
2014-01-12 16:27:13 +0100

    Avoid notices about new version

--- includes/version.php
+++ includes/version.php
@@ -3,7 +3,7 @@
  * YOURLS version
  *
  */
-define( 'YOURLS_VERSION', '1.7-RC1' );
+define( 'YOURLS_VERSION', '2.0-alpha-dont-use' );
 
 /**
  * YOURLS DB version. Increments when changes are made to the DB schema, to trigger a DB update
