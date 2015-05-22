8ef521ea37764e927af77f574a7200ee43494be4
ozh <ozh@ozh.org>
2015-05-22 23:59:44 +0200

    More tests.
    Makes functions-formatting.php covered at 93%

--- tests/format/general.php
+++ tests/format/general.php
@@ -39,64 +39,64 @@ class Format_General extends PHPUnit_Framework_TestCase {
         );
     }
 
-	/**
-	 * Check that yourls_is_serialized detects serialized data
-	 *
+    /**
+     * Check that yourls_is_serialized detects serialized data
+     *
      * @dataProvider serialize_data
-	 * @since 0.1
-	 */
-	public function test_is_serialized( $data ) {
-		$this->assertTrue( yourls_is_serialized( serialize( $data ) ) );
-	}
-	
-	/**
-	 * Check that yourls_is_serialized doesn't assume garbage is serialized
-	 *
+     * @since 0.1
+     */
+    public function test_is_serialized( $data ) {
+        $this->assertTrue( yourls_is_serialized( serialize( $data ) ) );
+    }
+    
+    /**
+     * Check that yourls_is_serialized doesn't assume garbage is serialized
+     *
      * @dataProvider not_serialized_data
-	 * @since 0.1
-	 */
-	public function test_is_not_serialized( $data ) {
+     * @since 0.1
+     */
+    public function test_is_not_serialized( $data ) {
         $this->assertFalse( yourls_is_serialized( $data ) );
-	}
-	
-	/**
-	 * Integer (1337) to string (3jk) to integer
-	 *
-	 * @since 0.1
-	 */
-	public function test_int_to_string_to_int() {
-		// 10 random integers
-		$rnd = array();
-		for( $i=0; $i<10; $i++ ) {
-			$rnd[]= mt_rand( 1, 1000000 );
-		}
-	
-		foreach( $rnd as $integer ) {
-			$this->assertEquals( $integer, yourls_string2int( yourls_int2string( $integer ) ) );
-		}
-	
-	}
+    }
+    
+    /**
+     * Integer (1337) to string (3jk) to integer
+     *
+     * @since 0.1
+     */
+    public function test_int_to_string_to_int() {
+        // 10 random integers
+        $rnd = array();
+        for( $i=0; $i<10; $i++ ) {
+            $rnd[]= mt_rand( 1, 1000000 );
+        }
+    
+        foreach( $rnd as $integer ) {
+            $this->assertEquals( $integer, yourls_string2int( yourls_int2string( $integer ) ) );
+        }
+    
+    }
 
-	/**
-	 * String (3jk) to integer (1337) to string
-	 *
-	 * @since 0.1
-	 */
-	public function test_string_to_int_to_string() {
-		// 10 random strings that do not start with a zero
-		$rnd = array();
-		$i = 0;
-		while( $i < 10 ) {
-			if( $notempty = ltrim( rand_str( mt_rand( 2, 10 ) ), '0' ) ) {
-				$rnd[]= $notempty;
-				$i++;
-			}
-		}
-	
-		foreach( $rnd as $string ) {
-			$this->assertEquals( $string, yourls_int2string( yourls_string2int( $string ) ) );
-		}
-	}
+    /**
+     * String (3jk) to integer (1337) to string
+     *
+     * @since 0.1
+     */
+    public function test_string_to_int_to_string() {
+        // 10 random strings that do not start with a zero
+        $rnd = array();
+        $i = 0;
+        while( $i < 10 ) {
+            if( $notempty = ltrim( rand_str( mt_rand( 2, 10 ) ), '0' ) ) {
+                $rnd[]= $notempty;
+                $i++;
+            }
+        }
+    
+        foreach( $rnd as $string ) {
+            $this->assertEquals( $string, yourls_int2string( yourls_string2int( $string ) ) );
+        }
+    }
     
     /**
      * Some random keywords
@@ -111,21 +111,21 @@ class Format_General extends PHPUnit_Framework_TestCase {
         );
     }
 
-	/**
-	 * Checking that string2htmlid is an alphanumeric string
-	 *
+    /**
+     * Checking that string2htmlid is an alphanumeric string
+     *
      * @dataProvider some_random_keywords
-	 * @since 0.1
-	 */
+     * @since 0.1
+     */
     public function test_string2htmlid( $string ) {
         $this->assertTrue( ctype_alnum( yourls_string2htmlid( $string ) ) );
     }
 
-	/**
-	 * Generating valid regexp from the allowed charset
-	 *
-	 * @since 0.1
-	 */
+    /**
+     * Generating valid regexp from the allowed charset
+     *
+     * @since 0.1
+     */
     function test_valid_regexp() {
         $pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
         
@@ -141,11 +141,11 @@ class Format_General extends PHPUnit_Framework_TestCase {
         $this->assertFalse( preg_match( '![^' . $pattern . ']!', null ) === false );
     }
     
-	/**
-	 * Trim long strings
-	 *
-	 * @since 0.1
-	 */
+    /**
+     * Trim long strings
+     *
+     * @since 0.1
+     */
     function test_trim_long_strings() {
         $long = "The Plague That Makes Your Booty Move... It's The Infectious Grooves";
         $trim = "The Plague That Makes Your Booty Move... It's The Infec[...]";
@@ -160,26 +160,26 @@ class Format_General extends PHPUnit_Framework_TestCase {
         $this->assertSame( $trim, yourls_trim_long_string( $long, 37, '..' ) );
     }
     
-	/**
-	 * Return true for UTF8 strings
+    /**
+     * Return true for UTF8 strings
      *
      * Note: As of 1.7.1, function yourls_seem_utf8() is still unused. In 2.0 consider simply deleting it if still not needed
-	 *
-	 * @dataProvider valid_utf8
-	 * @since 0.1
-	 */
+     *
+     * @dataProvider valid_utf8
+     * @since 0.1
+     */
     function test_is_utf8( $string ) {
         $this->assertTrue( yourls_seems_utf8( $string ) );
     }
  
-	/**
-	 * Return false for non UTF8 strings
+    /**
+     * Return false for non UTF8 strings
      *
      * Note: As of 1.7.1, function yourls_seem_utf8() is still unused. In 2.0 consider simply deleting it if still not needed
-	 *
-	 * @dataProvider invalid_utf8
-	 * @since 0.1
-	 */
+     *
+     * @dataProvider invalid_utf8
+     * @since 0.1
+     */
     function test_is_not_utf8( $string ) {
         $this->assertFalse( yourls_seems_utf8( $string ) );
     }
@@ -204,30 +204,85 @@ class Format_General extends PHPUnit_Framework_TestCase {
         return $strings;
     }
  
-	/**
-	 * Test yourls_backslashit
+    /**
+     * Test yourls_backslashit
      *
-	 * @since 0.1
-	 */
+     * @since 0.1
+     */
     function test_backslashit() {
         $this->assertSame( yourls_backslashit( 'hello world 123 !' ), '\h\e\l\l\o \w\o\r\l\d 123 !' );
         $this->assertSame( yourls_backslashit( '1, 2, 3' ), '\\\1, 2, 3' );
     }
     
-	/**
-	 * Test the bookmarklet generator
+    /**
+     * Test the bookmarklet generator
      *
      * Note: we're not testing that the bookmarklet generator produces valid JS code: the
      * bookmarklet class has tests for this, see https://github.com/ozh/bookmarkletgen
      * We're just testing that content is returned and that the bookmarklet class has been
      * loaded at some point
      *
-	 * @since 0.1
-	 */
+     * @since 0.1
+     */
     function test_bookmarklet() {
         $code = yourls_make_bookmarklet( 'hello' );
         $this->assertTrue( is_string( $code ) );
         $this->assertTrue( class_exists( '\BookmarkletGen', false ) );
     }
     
+    /**
+     * Test yourls_specialchars basics
+     *
+     * @since 0.1
+     */
+    function test_specialchars_decode_basics() {
+        $html =  "&amp;&lt;hello world&gt;";
+        $this->assertEquals( $html, yourls_specialchars( $html ) );
+
+        $double = "&amp;amp;&amp;lt;hello world&amp;gt;";
+        $this->assertEquals( $double, yourls_specialchars( $html, ENT_NOQUOTES, true ) );
+    }
+
+    /**
+     * Test yourls_specialchars escape quotes
+     *
+     * @since 0.1
+     */
+    function test_specialchars_escapes_quotes() {
+        $source = "\"'hello!'\"";
+        $this->assertEquals( '"&#039;hello!&#039;"', yourls_specialchars( $source, 'single' ) );
+        $this->assertEquals( "&quot;'hello!'&quot;", yourls_specialchars( $source, 'double' ) );
+        $this->assertEquals( '&quot;&#039;hello!&#039;&quot;', yourls_specialchars( $source, ENT_QUOTES, true ) );
+        $this->assertEquals( '&quot;&#039;hello!&#039;&quot;', yourls_specialchars( $source, 'omg', true ) ); // unaccepted value should be treated as ENT_QUOTES
+        $this->assertEquals( $source, yourls_specialchars( $source ) );
+    }
+
+    /**
+     * Test yourls_specialchars doesn't change allowed entities
+     *
+     * @since 0.1
+     */
+    function test_specialchars_allowed_entities() {
+        global $yourls_allowedentitynames;
+        foreach ( $yourls_allowedentitynames as $ent ) {
+            $ent = '&' . $ent . ';';
+            $this->assertEquals( $ent, yourls_specialchars( $ent ) );
+        }
+    }
+
+    /**
+     * Test yourls_specialchars with unallowed entities
+     *
+     * @since 0.1
+     */
+    function test_specialchars_unallowed_entities() {
+        $ents = array( 'iacut', 'aposs', 'pos', 'apo', 'apo?', 'apo.*', '.*apo.*', 'apos ', ' apos', ' apos ' );
+
+        foreach ( $ents as $ent ) {
+            $escaped = '&amp;' . $ent . ';';
+            $ent = '&' . $ent . ';';
+            $this->assertEquals( $escaped, yourls_specialchars( $ent ) );
+        }
+    }
+
 }
