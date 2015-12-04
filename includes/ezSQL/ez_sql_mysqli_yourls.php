6c003a91be33ffb49b48f8de463583d6cc3a941a
ozh <ozh@ozh.org>
2015-12-04 19:24:54 +0100

    More PHP7 fixes

--- includes/ezSQL/ez_sql_mysqli_yourls.php
+++ includes/ezSQL/ez_sql_mysqli_yourls.php
@@ -18,7 +18,7 @@ class ezSQL_mysqli_YOURLS extends ezSQL_mysqli {
      */
     function __construct( $user, $pass, $name, $host ) {
         $this->show_errors = defined( 'YOURLS_DEBUG' ) && YOURLS_DEBUG;
-        parent::ezSQL_mysqli( $user, $pass, $name, $host );
+        parent::__construct( $user, $pass, $name, $host );
     }
 	
 	/**
