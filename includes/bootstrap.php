8db6c144c86109b8c2dfd9f739e7009b65f05594
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2017-04-16 17:54:25 +0200

    More error compatibility

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -88,10 +88,11 @@ $yourls_user_passwords = array(
 
 // PHPUnit 6 compatibility for previous versions
 if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
-    class_alias( 'PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase' );
-    class_alias( 'PHPUnit\Framework\Assert', 'PHPUnit_Framework_Assert' );
-    class_alias( 'PHPUnit\Framework\Error\Error', 'PHPUnit_Framework_Error' );
+    class_alias( 'PHPUnit\Framework\Assert',        'PHPUnit_Framework_Assert' );
+    class_alias( 'PHPUnit\Framework\TestCase',      'PHPUnit_Framework_TestCase' );
+    class_alias( 'PHPUnit\Framework\Error\Error',   'PHPUnit_Framework_Error' );
+    class_alias( 'PHPUnit\Framework\Error\Notice',  'PHPUnit_Framework_Error_Notice' );
+    class_alias( 'PHPUnit\Framework\Error\Warning', 'PHPUnit_Framework_Error_Warning' );
 }
 
 // At this point, tests will start
-
