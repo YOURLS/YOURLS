57ad4e5fd341228498ee403f27e46e7409a94228
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2017-04-17 22:36:22 +0200

    Use PHP_VERSION_ID
    This makes calls to phpversion() and version_compare() unnecessary. Fixes #1951

--- includes/functions-install.php
+++ includes/functions-install.php
@@ -40,11 +40,11 @@ function yourls_get_database_version() {
 }
 
 /**
- * Check if PHP > 5.2
+ * Check if PHP > 5.3
  *
  */
 function yourls_check_php_version() {
-	return ( version_compare( '5.2', phpversion() ) <= 0 );
+    return(defined('PHP_VERSION_ID') && PHP_VERSION_ID > 50300);
 }
 
 /**
