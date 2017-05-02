6226cf1dc8af44ff2384efbc7e1aa36cd1bf9e51
ozh <ozh@ozh.org>
2017-05-02 21:25:55 +0200

    Bump for next dot release

--- includes/version.php
+++ includes/version.php
@@ -3,7 +3,7 @@
  * YOURLS version
  *
  */
-define( 'YOURLS_VERSION', '1.7.2' );
+define( 'YOURLS_VERSION', '1.7.3' );
 
 /**
  * YOURLS DB version. Increments when changes are made to the DB schema, to trigger a DB update
