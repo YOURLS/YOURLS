83120039ce4583d20fcf04c887f007642f374b1e
ozh <ozh@ozh.org>
2017-09-14 15:53:56 +0200

    Refactor test bootstrapping
    [skip ci]

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -3,81 +3,44 @@
  * YOURLS Unit Test. No, I don't know what I'm doing.
  */
 
-require_once dirname( __FILE__ ) . '/utils.php';
-require_once dirname( __FILE__ ) . '/install.php';
-
-// Include config
-$config_locations = array(
-	dirname( dirname( __FILE__ ) ) . '/yourls-tests-config.php',        // manual, run locally
-	dirname( dirname( dirname( __FILE__ ) ) ) . '/user/config.php',     // Travis, run from YOURLS/YOURLS
-	dirname( dirname( __FILE__ ) ) . '/yourls-tests-config-travis.php', // Travis, run from YOURLS/YOURLS-unit-tests
-);
-foreach( $config_locations as $config ) {
-	if( is_readable( $config ) ) {
-		define( 'YOURLS_CONFIGFILE', $config );
-		require_once YOURLS_CONFIGFILE;
-		break;
-	}
-}
-if( !defined( 'YOURLS_CONFIGFILE' ) ) {
-	die( sprintf( "ERROR: config file missing. Current directory: %s\n", dirname( __FILE__ ) ) );
-}
-
 // Globalize some YOURLS variables because PHPUnit loads this inside a function
 // See https://github.com/sebastianbergmann/phpunit/issues/325
+// This has to be done before including any file
 global $ydb, $yourls_user_passwords, $yourls_reserved_URL,        // main object & config file
        $yourls_filters, $yourls_actions,                          // used by plugin API
        $yourls_locale, $yourls_l10n, $yourls_locale_formats,      // used by L10N API
        $yourls_allowedentitynames, $yourls_allowedprotocols;      // used by KSES
 
+require_once dirname( __FILE__ ) . '/utils.php';
+require_once dirname( __FILE__ ) . '/install.php';
 
-// Initialize ourselves some constants that are typically user defined
+// Include relevant config file
+define('YOURLS_CONFIGFILE', yut_find_config());
+require_once YOURLS_CONFIGFILE;
 
-$yourls_reserved_URL = array(
-	'porn', 'sex', 'nigger', 'fuck', 'cunt', 'dick',
-);
-$yourls_user_consts = array(
-	'YOURLS_HOURS_OFFSET'     => 5,
-	'YOURLS_UNIQUE_URLS'      => true,
-	'YOURLS_PRIVATE'          => true,
-	'YOURLS_COOKIEKEY'        => 'I &hearts; unit tests',
-	'YOURLS_URL_CONVERT'      => 62,
-	'YOURLS_DB_PREFIX'        => 'yourls_',
-	'YOURLS_NO_HASH_PASSWORD' => true, // prevents rewriting config.php with encrypted passwords
-    'YOURLS_LANG'             => 'fr_FR', // locale of a sample translation file in the data dir
-);
-foreach( $yourls_user_consts as $CONST => $value ) {
-	if( !defined( $CONST ) )
-		define( $CONST, $value );
-}
+// Bootstrap YOURLS
+require_once YOURLS_ABSPATH . '/includes/vendor/autoload.php';
+define('YOURLS_TESTDATA_DIR', dirname( dirname( __FILE__ ) ) . '/data');
+define('YOURLS_LANG_DIR', YOURLS_TESTDATA_DIR.'/pomo');
+$config = new \YOURLS\Config\Config(YOURLS_CONFIGFILE);
+$config->define_core_constants();
 
-define( 'YOURLS_TESTDATA_DIR', dirname( dirname( __FILE__ ) ) . '/data' );
+// Define YOURLS actions upon new instance
+$init = new \YOURLS\Config\InitDefaults;
+$init->check_maintenance_mode        = false;
+$init->fix_request_uri               = false;
+$init->redirect_ssl                  = false;
+$init->redirect_to_install           = false;
+$init->check_if_upgrade_needed       = false;
+$init->load_plugins                  = false; // do not attempt at loading (no DB yet to store data), but do send the 'plugins_loaded' action (some code depend on it)
+$init->get_all_options               = false;
+$init->check_new_version             = false;
+$init->include_auth_funcs            = true;
+$init->include_install_upgrade_funcs = true;
+new \YOURLS\Config\Init($init);
 
-// All set -- go.
-yut_declare_yourls_consts();
-yut_load_yourls();
-yut_drop_all_tables_if_local();
+// All set -- install
 yut_install_yourls();
-
-// Compute 1 md5 and 2 phpass hashed passwords from a random password
-$random_password = rand_str();
-$salt = rand( 10000, 99999 );
-$md5  = 'md5:' . $salt . ':' . md5( $salt . $random_password );
-$phpassword_1 = 'phpass:' . str_replace( '$', '!', yourls_phpass_hash( $random_password ) );
-$phpassword_2 = 'phpass:' . yourls_phpass_hash( $random_password );
-
-$yourls_user_passwords = array(
-	'yourls'  => 'travis-ci-test',
-	'clear'   => 'somepassword',
-	'md5'     => $md5,          // password: $random_password
-	'phpass'  => $phpassword_1, // password: $random_password
-	'phpass2' => $phpassword_2, // password: also $random_password but without YOURLS internal char substitution
-    '1994'    => '@$*',
-    'special' => 'lol .\+*?[^]$(){}=!<>|:-/',
-    'quote1'  => '"ahah"',
-    'quote2'  => "'ahah'",
-    'utf8fun' => 'أنا أحب النقانق',
-);
 
 // PHPUnit 6 compatibility for previous versions
 if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
@@ -89,3 +52,4 @@ if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner
 }
 
 // At this point, tests will start
+echo "YOURLS installed, starting PHPUnit\n\n";
--- includes/install.php
+++ includes/install.php
@@ -1,181 +1,58 @@
 <?php
 
 /**
- * Install YOURLS
+ * Install YOURLS tables
  *
- * This function does the strict minimum to install YOURLS : check requirements and create the DB schema
- *
- * @since 0.1
+ * @return void
  */
 function yut_install_yourls() {
-	if( ! yourls_check_database_version() ) {
+    yut_drop_all_tables_if_local();
+    
+	if (!yourls_check_database_version()) {
 		die( sprintf( 'MySQL version too old. Version is: %s', yourls_get_database_version() ) ); 
 	}
 	
-	if( ! yourls_check_php_version() ) {
+	if (!yourls_check_php_version()) {
 		die( sprintf( 'PHP version too old. Version is: %s', phpversion() ) ); 
 	}
 		
 	$create = yourls_create_sql_tables();
-	if( array() != $create['error'] ) {
+	if (array() != $create['error']) {
 		die( sprintf( 'Could not run SQL. Error is: %s', implode( "\n\n", $create['error'] ) ) ); 
 	}
 }
 
-/**
- * Declare all needed YOURLS constants
- *
- * @since 0.1
- */
-function yut_declare_yourls_consts() {
-	// physical path of YOURLS root
-	if( !defined( 'YOURLS_ABSPATH' ) )
-		define( 'YOURLS_ABSPATH', str_replace( '\\', '/', dirname( dirname( __FILE__ ) ) ) );
-
-	// physical path of includes directory
-	if( !defined( 'YOURLS_INC' ) )
-		define( 'YOURLS_INC', YOURLS_ABSPATH.'/includes' );
-
-	// physical path of user directory
-	if( !defined( 'YOURLS_USERDIR' ) )
-		define( 'YOURLS_USERDIR', YOURLS_ABSPATH.'/user' );
-
-	// URL of user directory
-	if( !defined( 'YOURLS_USERURL' ) )
-		define( 'YOURLS_USERURL', YOURLS_SITE.'/user' );
-		
-	// physical path of asset directory
-	if( !defined( 'YOURLS_ASSETDIR' ) )
-		define( 'YOURLS_ASSETDIR', YOURLS_ABSPATH.'/assets' );
-
-	// URL of asset directory
-	if( !defined( 'YOURLS_ASSETURL' ) )
-		define( 'YOURLS_ASSETURL', YOURLS_SITE.'/assets' );
-		
-	// physical path of translations directory
-	if( !defined( 'YOURLS_LANG_DIR' ) )
-		define( 'YOURLS_LANG_DIR', YOURLS_TESTDATA_DIR.'/pomo' );
-
-	// physical path of plugins directory
-	if( !defined( 'YOURLS_PLUGINDIR' ) )
-		define( 'YOURLS_PLUGINDIR', YOURLS_USERDIR.'/plugins' );
-
-	// URL of plugins directory
-	if( !defined( 'YOURLS_PLUGINURL' ) )
-		define( 'YOURLS_PLUGINURL', YOURLS_USERURL.'/plugins' );
-		
-	// physical path of themes directory
-	if( !defined( 'YOURLS_THEMEDIR' ) )
-		define( 'YOURLS_THEMEDIR', YOURLS_USERDIR.'/themes' );
-
-	// URL of themes directory
-	if( !defined( 'YOURLS_THEMEURL' ) )
-		define( 'YOURLS_THEMEURL', YOURLS_USERURL.'/themes' );
-
-	// physical path of pages directory
-	if( !defined( 'YOURLS_PAGEDIR' ) )
-		define('YOURLS_PAGEDIR', YOURLS_ABSPATH.'/pages' );
-
-	// table to store URLs
-	if( !defined( 'YOURLS_DB_TABLE_URL' ) )
-		define( 'YOURLS_DB_TABLE_URL', YOURLS_DB_PREFIX.'url' );
-
-	// table to store options
-	if( !defined( 'YOURLS_DB_TABLE_OPTIONS' ) )
-		define( 'YOURLS_DB_TABLE_OPTIONS', YOURLS_DB_PREFIX.'options' );
-
-	// table to store hits, for stats
-	if( !defined( 'YOURLS_DB_TABLE_LOG' ) )
-		define( 'YOURLS_DB_TABLE_LOG', YOURLS_DB_PREFIX.'log' );
-
-	// minimum delay in sec before a same IP can add another URL. Note: logged in users are not throttled down.
-	if( !defined( 'YOURLS_FLOOD_DELAY_SECONDS' ) )
-		define( 'YOURLS_FLOOD_DELAY_SECONDS', 0 );
-
-	// comma separated list of IPs that can bypass flood check.
-	if( !defined( 'YOURLS_FLOOD_IP_WHITELIST' ) )
-		define( 'YOURLS_FLOOD_IP_WHITELIST', '' );
-
-	// life span of an auth cookie in seconds (60*60*24*7 = 7 days)
-	if( !defined( 'YOURLS_COOKIE_LIFE' ) )
-		define( 'YOURLS_COOKIE_LIFE', 60*60*24*7 );
-
-	// life span of a nonce in seconds
-	if( !defined( 'YOURLS_NONCE_LIFE' ) )
-		define( 'YOURLS_NONCE_LIFE', 43200 ); // 3600 * 12
-
-	// if set to true, disable stat logging (no use for it, too busy servers, ...)
-	if( !defined( 'YOURLS_NOSTATS' ) )
-		define( 'YOURLS_NOSTATS', false );
-
-	// if set to true, force https:// in the admin area
-	if( !defined( 'YOURLS_ADMIN_SSL' ) )
-		define( 'YOURLS_ADMIN_SSL', false );
-
-	// if set to true, verbose debug infos. Will break things. Don't enable.
-	if( !defined( 'YOURLS_DEBUG' ) )
-		define( 'YOURLS_DEBUG', true );
-		
-	if( !defined( 'YOURLS_ADMIN_LOCATION' ) )
-		define( 'YOURLS_ADMIN_LOCATION', 'admin' );
-		
-	// Error reporting
-	if( defined( 'YOURLS_DEBUG' ) && YOURLS_DEBUG == true ) {
-		error_reporting( -1 );
-	} else {
-		error_reporting( E_ERROR | E_PARSE );
-	}
-}
 
 /**
- * Load YOURLS
+ * Find YOURLS config depending on unit test context
  *
- * This is everything from load-yourls.php except:
- * - the config.php inclusion
- * - the maintenance mode check
- * - the "redirect to https:// if applicable" part
- * - checks for need to install or update
- * Yeah -- this is rather lame. Need to avoid to duplicate so much code.
+ * Unit tests can be run locally (someone typing 'phpunit' in their console) or on Travis
+ * on YOURLS/YOURLS or YOURLS/YOURLS/YOURLS-unit-tests when pushing commits.
  *
- * @since 0.1
+ * @return string  config file path (eg '/home/you/path/to/config.php')
  */
-function yut_load_yourls() {
-	// Include all functions
-    require_once YOURLS_INC. '/vendor/autoload.php';
-	require_once YOURLS_INC. '/version.php';
-	$files = scandir( YOURLS_INC, 1 );
-	foreach ( $files as $file ) {
-		if ( strpos( $file, 'functions' ) === 0 && file_exists( YOURLS_INC . '/' . $file ) ) {
-			require_once YOURLS_INC . '/' . $file;
-		}
-	}
-
-	// Create the YOURLS object $ydb that will contain everything we globally need
-	global $ydb;
-
-	// Allow drop-in replacement for the DB engine
-	if( file_exists( YOURLS_USERDIR.'/db.php' ) ) {
-		require_once YOURLS_USERDIR.'/db.php';
-	} else {
-		require_once YOURLS_INC.'/class-mysql.php';
-		yourls_db_connect();
-	}
-
-	// Allow early inclusion of a cache layer
-	if( file_exists( YOURLS_USERDIR.'/cache.php' ) )
-		require_once YOURLS_USERDIR.'/cache.php';
-		
-	// Init complete. We need to mimick the "plugins_loaded" event to load the kses lib
-	yourls_do_action( 'plugins_loaded' );
+function yut_find_config() {
+    $config_locations = array(
+        dirname(__DIR__). '/yourls-tests-config.php',         // manual, run locally
+        dirname(dirname(__DIR__)) . '/user/config.php',       // Travis, run from YOURLS/YOURLS
+        dirname(__DIR__) . '/yourls-tests-config-travis.php', // Travis, run from YOURLS/YOURLS-unit-tests
+    );
+    
+    foreach($config_locations as $config) {
+        if (is_readable($config)) {
+            return str_replace('\\', '/', $config);
+        }
+    }
+    
+    die( sprintf( "ERROR: config file missing. Current directory: %s\n", dirname(__DIR__) ) );
 }
-
 
 /**
  * Destroy tables in selected DB if tests run locally
  *
  * If not running in Travis environment, this function will drop all tables in the selected DB
  *
- * @since 0.1
+ * @return void
  */
 function yut_drop_all_tables_if_local() {
 	if( !yut_is_local() )
@@ -183,17 +60,7 @@ function yut_drop_all_tables_if_local() {
 
 	// If not running in Travis environment, drop any tables from the selected database prior to starting tests
 	global $ydb;
-	$sql = sprintf( "SELECT group_concat(table_name) FROM information_schema.tables WHERE table_schema = '%s';", YOURLS_DB_NAME );
-	try {
-		$tables = $ydb->fetchValue( $sql );
-	} catch( Exception $e ) {
-		return;
-	}
-	if( $tables ) {
-		try {
-			$drop = $ydb->fetchValue( sprintf( 'DROP TABLE %s', $tables ) );
-		} catch( Exception $e ) {
-			return;
-		}
-	}
+    $tables = sprintf('%s,%s,%s', YOURLS_DB_TABLE_URL, YOURLS_DB_TABLE_OPTIONS, YOURLS_DB_TABLE_LOG);
+    $sql = sprintf('DROP TABLE IF EXISTS %s', $tables);
+    $ydb->perform($sql);
 }
