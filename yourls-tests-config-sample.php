9b23bd5c22a5249ac918d402c687504169d4f037
ozh <ozh@ozh.org>
2017-09-14 16:00:12 +0200

    Travis & sample config
    [skip ci]

--- yourls-tests-config-sample.php
+++ yourls-tests-config-sample.php
@@ -18,3 +18,33 @@ define( 'YOURLS_DB_HOST', 'localhost' );
 /*** PHP binary - edit if the executable binary is not in system path and put full path ie 'c:/php/php.exe' */
 define( 'YOURLS_PHP_BIN', 'php' );
 
+/*** Most likely, don't edit anything else. Pretty much standard YOURLS config. */
+
+define('YOURLS_HOURS_OFFSET', 5);
+define('YOURLS_UNIQUE_URLS',  true);
+define('YOURLS_PRIVATE',  true);
+define('YOURLS_COOKIEKEY',  'I &hearts; unit tests');
+define('YOURLS_URL_CONVERT',  62);
+define('YOURLS_DB_PREFIX',  'yourls_');
+define('YOURLS_FLOOD_DELAY_SECONDS',  0);
+define('YOURLS_FLOOD_IP_WHITELIST',  '');
+define('YOURLS_NO_HASH_PASSWORD',  true); // prevents rewriting config.php with encrypted passwords
+define('YOURLS_LANG',  'fr_FR'); // locale of a sample translation file in the data dir
+define('YOURLS_DEBUG', true);
+
+$yourls_reserved_URL = array(
+	'porn', 'sex', 'nigger', 'fuck', 'cunt', 'dick',
+);
+
+$yourls_user_passwords = array(
+	'yourls'  => 'travis-ci-test',
+	'clear'   => 'somepassword',
+	'md5'     => 'md5:12373:e52e4488f79a740bd341f229e3c163c8',                          // password: '3cd6944201fa7bbc5e0fe852e36b1096' with md5 and salt
+	'phpass'  => 'phpass:!2a!08!T1ptMlBSxu7g3odpbUXgd.9wbKvg8k7cJt.HbwSqUNrlLPudWnf/6', // password: '3cd6944201fa7bbc5e0fe852e36b1096' with PHPass
+	'phpass2' => 'phpass:$2a$08$gt2bnpfUyuCX3hrp0RPOieFR1RwBnLsMzpq/NvPXwCdV3LqI3RGYi', // password: also '3cd6944201fa7bbc5e0fe852e36b1096' but without YOURLS internal char substitution
+    '1994'    => '@$*',
+    'special' => 'lol .\+*?[^]$(){}=!<>|:-/',
+    'quote1'  => '"ahah"',
+    'quote2'  => "'ahah'",
+    'utf8fun' => 'أنا أحب النقانق',
+);
