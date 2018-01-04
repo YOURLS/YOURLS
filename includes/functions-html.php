1546416dbefee8f21030f28d165eb14a5ba7eae6
Josh Panter <joshu@unfettered.net>
2018-01-03 19:21:11 -0500

    add filter to bypass yourls_html_addnew()

--- includes/functions-html.php
+++ includes/functions-html.php
@@ -176,6 +176,9 @@ function yourls_html_footer($can_query = true) {
  * @param string $keyword Keyword to prefill the input with
  */
 function yourls_html_addnew( $url = '', $keyword = '' ) {
+	$pre = yourls_apply_filter( 'shunt_html_addnew', false );
+		if ( false !== $pre )
+			return $pre;
 	?>
 	<main role="main">
 	<div id="new_url">
