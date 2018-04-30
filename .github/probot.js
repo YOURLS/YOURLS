db6bd644aad38138b1ec26c0713684bc08e8e8c0
Léo Colombaro <git@colombaro.fr>
2018-04-30 12:42:05 +0200

    Remove majordome-bot noise for long reply for now
    Would be relevant to introduce filter on username

--- .github/probot.js
+++ .github/probot.js
@@ -10,17 +10,16 @@ on('issues.opened')
 /**
  * Minimize comment length
  */
-on('issue_comment.created')
-  .filter(context => context.payload.comment.body
-    .trim()
-    .replace(/\s+/gi, ' ')
-    .split(' ')
-    .length > 200
-  )
-  .comment(`
-Thanks for your comment, @{{ sender.login }}!
-
-Your reply is very long.
-As a bot, I take less than a second to read it, but summing it up a little bit
-will help old-fashioned humans understand your thought.
-  `);
+// on('issue_comment.created')
+//   .filter(context => context.payload.comment.body
+//     .trim()
+//     .replace(/\s+/gi, ' ')
+//     .split(' ')
+//     .length > 200
+//   )
+//   .comment(`
+// Thanks for your comment, @{{ sender.login }}!
+// 
+// Your reply is very long.
+// As a bot, I take less than a second to read it, but summing it up a little bit will help old-fashioned humans understand your thought.
+//   `);
