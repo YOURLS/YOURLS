3ad1ff2a1a85e85565a3d1cbac86a22f11dc1841
Chris Seaton <bluemorbo@gmail.com>
2019-10-13 20:38:54 +0100

    Correct alignment to match standards

--- yourls-api.php
+++ yourls-api.php
@@ -44,7 +44,7 @@ if ( false === $return ) {
 if( isset( $_REQUEST['callback'] ) )
 	$return['callback'] = $_REQUEST['callback'];
 elseif ( isset( $_REQUEST['jsonp'] ) )
-    $return['callback'] = $_REQUEST['jsonp'];
+	$return['callback'] = $_REQUEST['jsonp'];
 
 $format = ( isset( $_REQUEST['format'] ) ? $_REQUEST['format'] : 'xml' );
 
