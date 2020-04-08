ba31e8ffbc809ba0ac7b30356e75f8aa2c82c234
Ozh <ozh@ozh.org>
2020-04-08 15:26:42 +0200

    Link to absolute URLs in admin/plugins.php
    Fixes #2606. Fixes #2605

--- admin/plugins.php
+++ admin/plugins.php
@@ -17,7 +17,7 @@ if( isset( $_GET['action'] ) ) {
 
 	// Check plugin file is valid
 	if( isset( $_GET['plugin'] ) && yourls_validate_plugin_file( YOURLS_PLUGINDIR.'/'.$_GET['plugin'].'/plugin.php') ) {
-		
+
 		global $ydb;
 		// Activate / Deactive
 		switch( $_GET['action'] ) {
@@ -27,14 +27,14 @@ if( isset( $_GET['action'] ) ) {
 					yourls_redirect( yourls_admin_url( 'plugins.php?success=activated' ), 302 );
 
 				break;
-		
+
 			case 'deactivate':
 				$result = yourls_deactivate_plugin( $_GET['plugin'].'/plugin.php' );
 				if( $result === true )
 					yourls_redirect( yourls_admin_url( 'plugins.php?success=deactivated' ), 302 );
 
 				break;
-				
+
 			default:
 				$result = yourls__( 'Unsupported action' );
 				break;
@@ -42,7 +42,7 @@ if( isset( $_GET['action'] ) ) {
 	} else {
 		$result = yourls__( 'No plugin specified, or not a valid plugin' );
 	}
-	
+
 	yourls_add_notice( $result );
 }
 
@@ -63,16 +63,16 @@ yourls_html_menu();
 
 	<main role="main">
 	<h2><?php yourls_e( 'Plugins' ); ?></h2>
-	
+
 	<?php
 	$plugins = (array)yourls_get_plugins();
 	uasort( $plugins, 'yourls_plugins_sort_callback' );
-	
+
 	$count = count( $plugins );
 	$plugins_count = sprintf( yourls_n( '%s plugin', '%s plugins', $count ), $count );
 	$count_active = yourls_has_active_plugins();
 	?>
-	
+
 	<p id="plugin_summary"><?php /* //translators: "you have '3 plugins' installed and '1' activated" */ yourls_se( 'You currently have <strong>%1$s</strong> installed, and <strong>%2$s</strong> activated', $plugins_count, $count_active ); ?></p>
 
 	<table id="main_table" class="tblSorter" cellpadding="0" cellspacing="1">
@@ -87,11 +87,11 @@ yourls_html_menu();
 	</thead>
 	<tbody>
 	<?php
-	
+
 	$nonce = yourls_create_nonce( 'manage_plugins' );
-	
+
 	foreach( $plugins as $file=>$plugin ) {
-		
+
 		// default fields to read from the plugin header
 		$fields = array(
 			'name'       => 'Plugin Name',
@@ -101,7 +101,7 @@ yourls_html_menu();
 			'author'     => 'Author',
 			'author_uri' => 'Author URI'
 		);
-		
+
 		// Loop through all default fields, get value if any and reset it
 		foreach( $fields as $field=>$value ) {
 			if( isset( $plugin[ $value ] ) ) {
@@ -111,19 +111,19 @@ yourls_html_menu();
 			}
 			unset( $plugin[$value] );
 		}
-		
+
 		$plugindir = trim( dirname( $file ), '/' );
-		
+
 		if( yourls_is_active_plugin( $file ) ) {
 			$class = 'active';
-			$action_url = yourls_nonce_url( 'manage_plugins', yourls_add_query_arg( array('action' => 'deactivate', 'plugin' => $plugindir ) ) );
+			$action_url = yourls_nonce_url( 'manage_plugins', yourls_add_query_arg( array('action' => 'deactivate', 'plugin' => $plugindir ), yourls_admin_url('plugins.php') ) );
 			$action_anchor = yourls__( 'Deactivate' );
 		} else {
 			$class = 'inactive';
-			$action_url = yourls_nonce_url( 'manage_plugins', yourls_add_query_arg( array('action' => 'activate', 'plugin' => $plugindir ) ) );
+			$action_url = yourls_nonce_url( 'manage_plugins', yourls_add_query_arg( array('action' => 'activate', 'plugin' => $plugindir ), yourls_admin_url('plugins.php') ) );
 			$action_anchor = yourls__( 'Activate' );
 		}
-			
+
 		// Other "Fields: Value" in the header? Get them too
 		if( $plugin ) {
 			foreach( $plugin as $extra_field=>$extra_value ) {
@@ -131,18 +131,18 @@ yourls_html_menu();
 				unset( $plugin[$extra_value] );
 			}
 		}
-		
+
 		$data['desc'] .= '<br/><small>' . yourls_s( 'plugin file location: %s', $file) . '</small>';
-		
+
 		printf( "<tr class='plugin %s'><td class='plugin_name'><a href='%s'>%s</a></td><td class='plugin_version'>%s</td><td class='plugin_desc'>%s</td><td class='plugin_author'><a href='%s'>%s</a></td><td class='plugin_actions actions'><a href='%s'>%s</a></td></tr>",
 			$class, $data['uri'], $data['name'], $data['version'], $data['desc'], $data['author_uri'], $data['author'], $action_url, $action_anchor
 			);
-		
+
 	}
 	?>
 	</tbody>
 	</table>
-	
+
 	<script type="text/javascript">
 	yourls_defaultsort = 0;
 	yourls_defaultorder = 0;
@@ -155,12 +155,12 @@ yourls_html_menu();
 		});
 	<?php } ?>
 	</script>
-	
+
 	<p><?php yourls_e( 'If something goes wrong after you activate a plugin and you cannot use YOURLS or access this page, simply rename or delete its directory, or rename the plugin file to something different than <code>plugin.php</code>.' ); ?></p>
-	
+
 	<h3><?php yourls_e( 'More plugins' ); ?></h3>
-	
+
 	<p><?php yourls_e( 'For more plugins, head to the official <a href="http://yourls.org/pluginlist">Plugin list</a>.' ); ?></p>
 	</main>
-	
+
 <?php yourls_html_footer(); ?>
