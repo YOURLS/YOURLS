235d639799ae993f3655c4af156f7b16f7e86782
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2020-04-21 16:34:38 +0200

    Bump 1.7.10

--- includes/version.php
+++ includes/version.php
@@ -3,7 +3,7 @@
  * YOURLS version
  *
  */
-define( 'YOURLS_VERSION', '1.7.9' );
+define( 'YOURLS_VERSION', '1.7.10' );
 
 /**
  * YOURLS DB version. Increments when changes are made to the DB schema, to trigger a DB update
