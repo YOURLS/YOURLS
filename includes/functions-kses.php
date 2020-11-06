YOURLS/YOURLS

commit 0926cc60db387e5d9d46b4e06fd8fe41fc635619

Add WhatsApp and Telegram Protocols (#2781)

includes/functions-kses.php
@@ -544,7 +544,7 @@ function yourls_kses_allowed_protocols() {
 		'irc://', 'ircs://', 'mumble://', 
 		'callto:', 'skype:', 'sip:',
 		'teamspeak://', 'tel:', 'ventrilo://', 'xfire:', 
-		'ymsgr:', 
+		'ymsgr:', 'tg://', 'whatsapp://'
 
 		// Misc
 		'steam:', 'steam://',

commit dd199ed55ebc11991630c1513b2225a22a2e87a9

Fix typo broken in #2781

includes/functions-kses.php
@@ -544,7 +544,7 @@ function yourls_kses_allowed_protocols() {
 		'irc://', 'ircs://', 'mumble://', 
 		'callto:', 'skype:', 'sip:',
 		'teamspeak://', 'tel:', 'ventrilo://', 'xfire:', 
-		'ymsgr:', 'tg://', 'whatsapp://'
+		'ymsgr:', 'tg://', 'whatsapp://',
 
 		// Misc
 		'steam:', 'steam://',