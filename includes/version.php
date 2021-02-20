a5550743e9434c600fb829c4b20aa905b3ebb378
Ozh <ozh@ozh.org>
2021-02-20 20:11:51 +0100

    Fix collation in the update. Bump DB to 506.
    Should fix #2766

--- includes/version.php
+++ includes/version.php
@@ -9,4 +9,4 @@ define( 'YOURLS_VERSION', '1.7.10' );
  * YOURLS DB version. Increments when changes are made to the DB schema, to trigger a DB update
  *
  */
-define( 'YOURLS_DB_VERSION', '505' );
+define( 'YOURLS_DB_VERSION', '506' );
