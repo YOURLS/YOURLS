346097e69e5671f13511f5aa6c531b62e063bd73
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2021-03-06 19:09:22 +0100

    Bump for 1.8.1
    [skip ci]

--- includes/version.php
+++ includes/version.php
@@ -3,7 +3,7 @@
  * YOURLS version
  *
  */
-define( 'YOURLS_VERSION', '1.8' );
+define( 'YOURLS_VERSION', '1.8.1' );
 
 /**
  * YOURLS DB version. Increments when changes are made to the DB schema, to trigger a DB update
