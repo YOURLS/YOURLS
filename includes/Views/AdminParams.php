93b2782501325197403ae8b16ba453e1e3eaf673
Ozh <ozh@ozh.org>
2021-04-27 22:16:37 +0200

    More comments & documentation

--- includes/Views/AdminParams.php
+++ includes/Views/AdminParams.php
@@ -8,36 +8,46 @@
 namespace YOURLS\Views;
 
 /**
- * Class Admin
+ * Class AdminParams to get admin view parameters (number of links to display, search, ...)
  *
+ * @since   1.8.2
  * @package YOURLS\Views
  */
 class AdminParams
 {
 
     /**
+     * All possible search parameters. Populated in the constructor.
+     *
      * @var array
      */
     private $possible_search_params;
 
     /**
+     * All possible sort parameters. Populated in the constructor.
+     *
      * @var array
      */
     private $possible_sort_params;
 
     /**
+     * All possible date sorting parameters. Populated in the constructor.
+     *
      * @var array
      */
     private $possible_date_sorting;
 
     /**
+     * Parameter translations. Populated in the constructor.
+     *
      * @var array
      */
     private $params_translations;
 
 
     /**
-     * Admin constructor.
+     * Admin constructor : populate all default parameters
+     *
      */
     public function __construct()
     {
@@ -57,6 +67,8 @@ class AdminParams
 
     /**
      * Get the number of links to display per page
+     *
+     * @since 1.8.2
      *
      * @param int $default default number of links to display
      * @return int
@@ -78,6 +90,8 @@ class AdminParams
     /**
      * Get the current page number to be displayed
      *
+     * @since 1.8.2
+     *
      * @return int
      */
     public function get_page(): int
@@ -92,6 +106,8 @@ class AdminParams
      * explodes the search text into protocol, slashes and the rest (see JS function
      * split_search_text_before_search()) and this function glues pieces back together
      * See issue https://github.com/YOURLS/YOURLS/issues/1576
+     *
+     * @since 1.8.2
      *
      * @return string
      */
@@ -115,6 +131,8 @@ class AdminParams
     /**
      * Get the 'Search In' parameter (one of 'all', 'keyword', 'url', 'title', 'ip')
      *
+     * @since 1.8.2
+     *
      * @return string
      */
     public function get_search_in(): string
@@ -130,6 +148,10 @@ class AdminParams
     }
 
     /**
+     * Get the 'Sort by' parameter
+     *
+     * @since 1.8.2
+     *
      * @return mixed
      */
     public function get_sort_by(): string
@@ -149,6 +171,8 @@ class AdminParams
      *
      * No checks : you need to supply an existing parameter, see $params_translations
      *
+     * @since 1.8.2
+     *
      * @param string $param
      * @return string
      */
@@ -158,6 +182,10 @@ class AdminParams
     }
 
     /**
+     * Get the sort order (asc or desc)
+     *
+     * @since 1.8.2
+     *
      * @return mixed
      */
     public function get_sort_order()
@@ -168,6 +196,10 @@ class AdminParams
     }
 
     /**
+     * Get the click "more or less than"
+     *
+     * @since 1.8.2
+     *
      * @return mixed
      */
     public function get_click_filter()
@@ -177,19 +209,28 @@ class AdminParams
     }
 
     /**
+     * Get the click threshold
+     *
+     * @since 1.8.2
+     *
      * @return int|string
      */
     public function get_click_limit()
     {
         // @hook Default link click threshold (unset)
-        return isset($_GET['click_limit']) && intval($_GET['click_limit']) ? intval($_GET['click_limit']) : yourls_apply_filter('admin_view_click_limit', '');
+        return (isset($_GET['click_limit']) && intval($_GET['click_limit'])) ?
+            intval($_GET['click_limit']) : yourls_apply_filter('admin_view_click_limit', '');
     }
 
 
     /**
+     * Get the date parameters : the date "filter" and the two dates
+     *
+     * @since 1.8.2
+     *
      * @return array
      */
-    public function get_date_params()
+    public function get_date_params(): array
     {
         if (isset($_GET['date_filter']) && in_array($_GET['date_filter'], $this->possible_date_sorting)) {
             $date_filter = $_GET['date_filter'];
