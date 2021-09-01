094bf9dca4539b2d12f11f9d344b5c72adc222ba
Ozh <ozh@ozh.org>
2021-09-01 12:24:39 +0200

    Refactor

--- includes/functions-auth.php
+++ includes/functions-auth.php
@@ -640,8 +640,34 @@ function yourls_verify_nonce( $action, $nonce = false, $user = false, $return = 
 /**
  * Check if YOURLS_USER comes from environment variables
  *
+ * @since 1.8.2
+ * @return bool     true if YOURLS_USER and YOURLS_PASSWORD are defined as environment variables
  */
-function yourls_check_user_from_env() {
-	if ( getenv('YOURLS_USER') && getenv('YOURLS_PASSWORD') && count($yourls_user_passwords) == 1 ) return true;
-	return false;
+function yourls_is_user_from_env() {
+	return yourls_apply_filter('is_user_from_env', getenv('YOURLS_USER') && getenv('YOURLS_PASSWORD'));
+
+}
+
+/**
+ * Check if we should hash passwords in the config file
+ *
+ * By default, passwords are hashed. They are not if
+ *    - there is no password in clear text in the config file (ie everything is already hashed)
+ *    - the user defined constant YOURLS_NO_HASH_PASSWORD is true, see https://github.com/YOURLS/YOURLS/wiki/Username-Passwords#but-i-dont-want-to-encrypt-my-password-
+ *    - YOURLS_USER and YOURLS_PASSWORD are provided by the environment, not the config file
+ *
+ * @since 1.8.2
+ * @return bool
+ */
+function yourls_maybe_hash_passwords() {
+    $hash = true;
+
+    if ( !yourls_has_cleartext_passwords()
+         OR (defined('YOURLS_NO_HASH_PASSWORD') && YOURLS_NO_HASH_PASSWORD)
+         OR (yourls_is_user_from_env())
+    ) {
+        $hash = false;
+    }
+
+    return yourls_apply_filter('maybe_hash_password', $hash );
 }
