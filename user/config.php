c8bbcfc65ab9a25cecfad56257cfbaab11d6152b
AnonSS <mysticguy9@yahoo.com>
2021-10-03 14:16:21 -0700

    Create config.php

---
+++ user/config.php
@@ -0,0 +1,7 @@
+define( 'YOURLS_DB_USER', 'yourlsdbuser' );
+define( 'YOURLS_DB_PASS', 'yourlsdbpasswd' );
+define( 'YOURLS_DB_NAME', 'yourlsdb' );
+define( 'YOURLS_SITE', 'http://test1.us.lsst.org' );
+define( 'YOURLS_COOKIEKEY', 'lkfjafmoi4jfsjfasfjlfgcggjkihdgjhgigjgitjaaewesfsdfsdogmbnsingfjjgdfolsfmweml' );
+$yourls_user_passwords = array(
+        'username123' => 'password123',
