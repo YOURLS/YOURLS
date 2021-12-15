07999e291b430fb177a270fd02724b19bcf88670
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-15 20:05:48 +0800

    Delete includes directory

--- includes/Exceptions/ConfigException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace YOURLS\Exceptions;
-
-class ConfigException extends \Exception {}
--- includes/vendor/maxmind/web-service-common/src/Exception/IpAddressNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace MaxMind\Exception;
-
-class IpAddressNotFoundException extends InvalidRequestException
-{
-}
