c72c3eba1150368036f597d3d5a374254adaf545
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2023-03-05 12:41:16 +0100

    Bump version
    [skip ci]

--- includes/version.php
+++ includes/version.php
@@ -9,7 +9,7 @@
  *    MAJOR.MINOR.PATCH-SOMETHING (1.8.1-donotuse)
  *
  */
-define( 'YOURLS_VERSION', '1.9.2' );
+define( 'YOURLS_VERSION', '1.9.3-dev' );
 
 /**
  * YOURLS DB version. Increments when changes are made to the DB schema, to trigger a DB update
