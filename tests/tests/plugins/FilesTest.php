42c66b0928d418499b513e481150908606ac0249
Léo Colombaro <LeoColomb@users.noreply.github.com>
2025-03-06 16:24:56 +0000

    Fix the tests eventually

--- tests/tests/plugins/FilesTest.php
+++ tests/tests/plugins/FilesTest.php
@@ -141,10 +141,9 @@ class FilesTest extends PHPUnit\Framework\TestCase {
     #[\PHPUnit\Framework\Attributes\Depends('test_plugin_activate')]
     public function test_plugin_deactivate( $plugin ) {
 		$this->assertTrue( yourls_deactivate_plugin($plugin) );
-        $this->markTestSkipped('Raising issues');
-		// $this->assertSame( 0, yourls_has_active_plugins() );
-		// $this->assertFalse( yourls_is_active_plugin($plugin) );
-		// return $plugin;
+		$this->assertSame( 0, yourls_has_active_plugins() );
+		$this->assertFalse( yourls_is_active_plugin($plugin) );
+		return $plugin;
 	}
 
     /**
