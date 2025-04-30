91153e06fd0e71cd6c82aee4c7966a10b702ad9e
Eduard Pech <80007889+SimStim@users.noreply.github.com>
2025-04-30 13:12:44 -0600

    Update index.php
    Message on zero URLs has been made more universal:
    - If no URLs in database, "No URLs."
    - If no URLs found while searching with filters, an additional recommendation to be less specific is added.
    
    Later it still outputs ", counting 0 clicks." - which I don't consider helpful in this particular case. But let's not lump everything into one big pull request. And the "proper" behaviour is not for me to decide anyway.
    
    So I think, my original idea "is done here."

--- admin/index.php
+++ admin/index.php
@@ -242,9 +242,12 @@ yourls_do_action( 'admin_page_before_content' );
 
 if ( !$is_bookmark ) { ?>
 	<p><?php echo $search_sentence; ?></p>
-	<p><?php
+    <p><?php
         if ($total_items === 0)
-            printf( yourls__( 'Displaying <strong>ZERO</strong> URLs, because none have been defined' ));
+        {
+            printf( yourls__( 'No URLs' ));
+            if ($search !== "") printf( yourls__( '. Try being less specific' ));
+        }
         else printf( yourls__( 'Display <strong>%1$s</strong> to <strong class="increment">%2$s</strong> of <strong class="increment">%3$s</strong> URLs' ), $display_on_page, $max_on_page, $total_items );
 		if( $total_items_clicks !== false )
 			echo ", " . sprintf( yourls_n( 'counting <strong>1</strong> click', 'counting <strong>%s</strong> clicks', $total_items_clicks ), yourls_number_format_i18n( $total_items_clicks ) );
