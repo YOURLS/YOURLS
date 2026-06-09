00f788a981e7595973418f63cf2b5eed5833baaa
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2026-06-09 20:22:20 +0200

    More flood tests to cover more cases
    And actually check the flood protection

--- tests/tests/shorturl/FloodTest.php
+++ tests/tests/shorturl/FloodTest.php
@@ -10,6 +10,12 @@ class FloodTest extends PHPUnit\Framework\TestCase {
     protected function tearDown(): void {
         yourls_remove_all_filters( 'get_flood_delay' );
         yourls_remove_all_filters( 'get_flood_ip_whitelist' );
+        yourls_remove_all_filters( 'shunt_check_IP_flood' );
+        yourls_remove_all_filters( 'is_installing' );
+        yourls_remove_all_filters( 'is_private' );
+        yourls_remove_all_filters( 'shunt_is_valid_user' );
+        yourls_remove_all_filters( 'get_IP' );
+        yourls_remove_all_actions( 'pre_yourls_die' );
     }
 
     /**
@@ -75,6 +81,66 @@ class FloodTest extends PHPUnit\Framework\TestCase {
         yourls_add_filter( 'get_flood_ip_whitelist', fn() => $list );
 
         $this->assertSame( $ok, yourls_get_flood_ip_whitelist() );
+    }
+
+    /**
+     * Function can be shunted
+     */
+    public function test_check_ip_flood_is_shuntable() {
+        yourls_add_filter( 'shunt_check_IP_flood', fn() => 'shunted' );
+        $this->assertSame( 'shunted', yourls_check_IP_flood( '1.2.3.4' ) );
+    }
+
+    /**
+     * No flood check while installing
+     */
+    public function test_check_ip_flood_passes_when_installing() {
+        yourls_add_filter( 'get_flood_delay', fn() => 666 );
+        yourls_add_filter( 'is_installing', 'yourls_return_true' );
+        $this->assertTrue( yourls_check_IP_flood( '1.2.3.4' ) );
+    }
+
+    /**
+     * Logged-in users are not throttled (private install + valid user)
+     */
+    public function test_check_ip_flood_passes_for_logged_in_user() {
+        yourls_add_filter( 'get_flood_delay', fn() => 666 );
+        yourls_add_filter( 'is_private', 'yourls_return_true' );
+        yourls_add_filter( 'shunt_is_valid_user', 'yourls_return_true' );
+
+        $this->assertTrue( yourls_check_IP_flood( '1.2.3.4' ) );
+    }
+
+    /**
+     * Whitelisted IPs are not throttled
+     */
+    public function test_check_ip_flood_passes_for_whitelisted_ip() {
+        yourls_add_filter( 'get_flood_delay', fn() => 666 );
+        yourls_add_filter( 'is_private', 'yourls_return_false' );
+        yourls_add_filter( 'get_flood_ip_whitelist', fn() => [ '1.2.3.4' ] );
+
+        $this->assertTrue( yourls_check_IP_flood( '1.2.3.4' ) );
+    }
+
+    /**
+     * A too-fast second request from the same IP triggers the flood guard (yourls_die)
+     */
+    public function test_check_ip_flood_dies_on_flood() {
+        $ip = '203.0.113.42';
+        yourls_add_filter( 'get_flood_delay', fn() => 3600 );
+        yourls_add_filter( 'is_private', 'yourls_return_false' );
+        yourls_add_filter( 'get_IP', fn() => $ip );
+
+        // A link was just added from this IP (recent timestamp)
+        yourls_insert_link_in_db( 'https://' . rand_str(), rand_str(), rand_str() );
+
+        // Intercept yourls_die() before it actually dies
+        yourls_add_action( 'pre_yourls_die', function() { throw new Exception( 'flood' ); } );
+
+        $this->expectException( Exception::class );
+        $this->expectExceptionMessage( 'flood' );
+
+        yourls_check_IP_flood( $ip );
     }
 
 }
